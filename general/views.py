4428b0d98d6e0422458887c8c4ec80f57bfa8513
SaddySk <sksaddamh08@gmail.com>
2021-07-24 21:44:03 +0530

    initial

--- general/views.py
+++ general/views.py
@@ -1,9 +1,51 @@
 from django.shortcuts import render
+from decouple import config
+from general import decode_jwt
+import base64
+import requests
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'index.html')
+    code = request.GET.get('code')
+    userData = getTokens(code)
+    return render(request, 'index.html', userData)
 
 
+def getTokens(code):
+    TOKEN_ENDPOINT = config('TOKEN_ENDPOINT')
+    REDIRECT_URI = config('REDIRECT_URI')
+    CLIENT_ID = config('CLIENT_ID')
+    CLIENT_SECRET = config('CLIENT_SECRET')
+
+    encodeData = base64.b64encode(
+        bytes(f"{CLIENT_ID}:{CLIENT_SECRET}", "ISO-8859-1")).decode("ascii")
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': f'Basic {encodeData}'
+    }
+
+    body = {
+        'grant_type': 'authorization_code',
+        'client_id': CLIENT_ID,
+        'code': code,
+        'redirect_uri': REDIRECT_URI
+    }
+
+    response = requests.post(TOKEN_ENDPOINT, data=body, headers=headers)
+    id_token = response.json()['id_token']
+
+    userData = decode_jwt.lambda_handler(id_token, None)
+
+    if not userData:
+        return False
+
+    user = {
+        'id_token': id_token,
+        'name': userData['name'],
+        'emai': userData['email'],
+    }
+    return user
